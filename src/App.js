95bcfa49d629196b70b938b41fb96ce8e05227f6
Ayush Jain <ayushjain1126@gmail.com>
2022-10-03 18:44:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
+import NavBar from './components/NavBar';
+import TextArea from './components/TextArea';
+import Home from './components/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <NavBar></NavBar> */}
+      {/* <TextArea title="Enter the text to analyze"></TextArea> */}
+
+      <Router>
+        <NavBar></NavBar>
+          <Routes>
+            <Route exact path="/" element={<TextArea  title="Enter the text to analyze" />} />
+            <Route exact path="/TextArea" element={<TextArea  title="Enter the text to analyze" />} />
+            <Route exact path="/AboutUs" element={<About />} />
+            <Route exact path="/Home" element={<Home />} />
+            {/* <Route path="*" element={<NotFound/>}/> */}
+          </Routes>
+          <Footer></Footer>
+      </Router>
+    </>
   );
 }
 
